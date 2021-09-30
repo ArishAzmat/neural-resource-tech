3f856fe742af4a8acf37a3896dfbb65691a86b62
Osama Islam <islamosama488@gmail.com>
2021-09-30 17:36:33 +0530

    aadding jquery with errors

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,34 +1,34 @@
 import React from 'react'
 import Technologies from './Technologies'
 import Testimonial from './Testimonial'
-import bannerImg from '../img/bann1.png';
-import contentImg from '../img/home-ab.png';
-import imgPhn from '../img/mobile.png';
+import bannerImg from '../assets/img/bann1.png';
+import contentImg from '../assets/img/home-ab.png';
+import imgPhn from '../assets/img/mobile.png';
 
 const Banner = () => {
     return (
         <>
             {/* BANNER SECTION */}
-            <section id="home" class="w3l-banner py-5">
-                <div class="banner-image">
+            <section id="home" className="w3l-banner py-5">
+                <div className="banner-image">
 
                 </div>
-                <div class="banner-content">
-                    <div class="container pt-5 pb-md-4 HomePageHeroHeader">
-                        <div class="row align-items-center">
-                            <div class="col-md-6 pt-md-0 pt-4">
-                                <h3 class="mb-lg-4 mb-3 title">
-                                    We Are <span class="typed-text"></span><span class="cursor">&nbsp</span></h3>
+                <div className="banner-content">
+                    <div className="container pt-5 pb-md-4 HomePageHeroHeader">
+                        <div className="row align-items-center">
+                            <div className="col-md-6 pt-md-0 pt-4">
+                                <h3 className="mb-lg-4 mb-3 title">
+                                    We Are <span className="typed-text"></span><span className="cursor">&nbsp</span></h3>
                                 <p>Our Company Neural Resource Technology helps companies to find experienced and skilled IT experts. We
                                     provide IT staffing solutions to you according to your requirement. We focus on what is best for your
                                     business or organization because your growth is our motto. We also make websites with the newest
                                     technologies and skilled IT experts.</p>
-                                <div class="mt-md-5 mt-4 mb-lg-0 mb-4">
-                                    <a class="btn button-style" href="services/about_us.html">Get Started</a>
+                                <div className="mt-md-5 mt-4 mb-lg-0 mb-4">
+                                    <a className="btn button-style" href="services/about_us.html">Get Started</a>
                                 </div>
                             </div>
-                            <div class="col-md-6 mt-md-0 mt-4">
-                                <img class="img-fluid" src={bannerImg} alt="banner-dsc" />
+                            <div className="col-md-6 mt-md-0 mt-4">
+                                <img className="img-fluid" src={bannerImg} alt="banner-dsc" />
                             </div>
                         </div>
                     </div>
@@ -38,16 +38,16 @@ const Banner = () => {
 
             {/* ABOUT SECTION */}
 
-            <div class="w3l-content-photo-5 py-5">
-                <div class="container py-lg-5 py-4 HomePageAbout">
-                    <div class="row align-items-center">
-                        <div class="col-md-6 content-photo order-md-first order-last">
-                            <img src={contentImg} class="img-fluid" alt="content-photo" />
+            <div className="w3l-content-photo-5 py-5">
+                <div className="container py-lg-5 py-4 HomePageAbout">
+                    <div className="row align-items-center">
+                        <div className="col-md-6 content-photo order-md-first order-last">
+                            <img src={contentImg} className="img-fluid" alt="content-photo" />
                         </div>
-                        <div class="col-md-6 content-left mb-md-0 mb-5 pl-lg-5 order-md-last order-first">
-                            <h3 class="mb-3">
+                        <div className="col-md-6 content-left mb-md-0 mb-5 pl-lg-5 order-md-last order-first">
+                            <h3 className="mb-3">
                                 Welcome To Our Website</h3>
-                            <p class="mt-3">By joining Neural Resource, you will not only take our resouce but also become a part of our
+                            <p className="mt-3">By joining Neural Resource, you will not only take our resouce but also become a part of our
                                 family. We give our
                                 resource as well as give a support from our side.
                                 We have tiered our staff to withstand even the toughest of conditions.
@@ -55,7 +55,7 @@ const Banner = () => {
                                 environments.We are a
                                 branch whose fruit is its staff and you are the one to taste it.
                                 Join neural resource for better future of your busssiness.</p>
-                            <a class="btn button-style mt-lg-5 mt-4" href="about-page/index.html">Learn More </a>
+                            <a className="btn button-style mt-lg-5 mt-4" href="about-page/index.html">Learn More </a>
                         </div>
                     </div>
                 </div>
@@ -68,16 +68,16 @@ const Banner = () => {
             <section></section>
 
             {/* EXPERTISE SECTION */}
-            <section class="w3l-features-4">
-                <div class="features4-block text-center py-5">
-                    <div class="container py-md-5 py-3 HomePageServices">
+            <section className="w3l-features-4">
+                <div className="features4-block text-center py-5">
+                    <div className="container py-md-5 py-3 HomePageServices">
 
-                        <div class="row features4-grids justify-content-center">
+                        <div className="row features4-grids justify-content-center">
 
-                            <div class="col-lg-3 col-md-6">
-                                <div class="features4-grid">
-                                    <div class="feature-images">
-                                        <span class="fas fa-business-time icon-color" aria-hidden="true"></span>
+                            <div className="col-lg-3 col-md-6">
+                                <div className="features4-grid">
+                                    <div className="feature-images">
+                                        <span className="fas fa-business-time icon-color" aria-hidden="true"></span>
                                     </div>
                                     <h5><a href="services-page/index.html">1. IT Development</a>
                                     </h5>
@@ -87,10 +87,10 @@ const Banner = () => {
                                 </div>
                             </div>
 
-                            <div class="col-lg-3 col-md-6 mt-md-0 mt-2">
-                                <div class="features4-grid">
-                                    <div class="feature-images">
-                                        <span class="fas fa-laptop-code icon-color" aria-hidden="true"></span>
+                            <div className="col-lg-3 col-md-6 mt-md-0 mt-2">
+                                <div className="features4-grid">
+                                    <div className="feature-images">
+                                        <span className="fas fa-laptop-code icon-color" aria-hidden="true"></span>
                                     </div>
                                     <h5><a href="services-page/index.html">2. IT Staffing</a>
                                     </h5>
@@ -99,10 +99,10 @@ const Banner = () => {
                                 </div>
                             </div>
 
-                            <div class="col-lg-3 col-md-6 mt-lg-0 mt-2">
-                                <div class="features4-grid">
-                                    <div class="feature-images">
-                                        <span class="fas fa-clipboard-check icon-color" aria-hidden="true"></span>
+                            <div className="col-lg-3 col-md-6 mt-lg-0 mt-2">
+                                <div className="features4-grid">
+                                    <div className="feature-images">
+                                        <span className="fas fa-clipboard-check icon-color" aria-hidden="true"></span>
                                     </div>
                                     <h5><a href="services-page/index.html">3. Permanent Hiring</a>
                                     </h5>
@@ -110,10 +110,10 @@ const Banner = () => {
                                         simply looking for information on predicted compensation..</p>
                                 </div>
                             </div>
-                            <div class="col-lg-3 col-md-6 mt-lg-0 mt-2">
-                                <div class="features4-grid">
-                                    <div class="feature-images">
-                                        <span class="fas fa-clipboard-check icon-color" aria-hidden="true"></span>
+                            <div className="col-lg-3 col-md-6 mt-lg-0 mt-2">
+                                <div className="features4-grid">
+                                    <div className="feature-images">
+                                        <span className="fas fa-clipboard-check icon-color" aria-hidden="true"></span>
                                     </div>
                                     <h5><a href="services-page/index.html">4. Training Program</a>
                                     </h5>
@@ -132,17 +132,17 @@ const Banner = () => {
 
             {/* MOBILE SECTION */}
 
-            <section class="w3l-promocode">
-                <div class="promo-block py-5">
-                    <div class="container py-md-5 py-4 HomePagePromocode">
-                        <div class="row aap-4-section">
-                            <div class="col-lg-6 app4-right-image">
-                                <img src={imgPhn} class="img-fluid" alt="App Device" />
+            <section className="w3l-promocode">
+                <div className="promo-block py-5">
+                    <div className="container py-md-5 py-4 HomePagePromocode">
+                        <div className="row aap-4-section">
+                            <div className="col-lg-6 app4-right-image">
+                                <img src={imgPhn} className="img-fluid" alt="App Device" />
                             </div>
-                            <div class="col-lg-6 app4-left-text pl-lg-0 mb-lg-0 mb-5">
+                            <div className="col-lg-6 app4-left-text pl-lg-0 mb-lg-0 mb-5">
                                 <h6>We are best in developing Mobile Applications </h6>
                                 <h4>Get quality applications with us </h4>
-                                <p class="mb-4">
+                                <p className="mb-4">
                                     Grow your business online with a suitable appllication with us </p>
                             </div>
                         </div>
--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,64 +1,66 @@
-import React from 'react';
+// import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAndroid, faJs, faLaravel, faPhp, faReact, faWordpress } from '@fortawesome/free-brands-svg-icons';
 import { faAppleAlt, faDatabase } from '@fortawesome/free-solid-svg-icons';
 
 const Technologies = () => {
     return (
-        <section class="features-section pt-5" id="work">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12 right-wthree-img align-self order-lg-last order-first">
-                        <div class="text-center mb-sm-5 mb-4">
-                            <h3 class="title-big">
-                                <span class="text-blue"> Our</span> <span class="icon-color">Technologies </span>
+        <section className="features-section pt-5" id="work">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12 right-wthree-img align-self order-lg-last order-first">
+                        <div className="text-center mb-sm-5 mb-4">
+                            <h3 className="title-big">
+                                <span className="text-blue"> Our</span> <span className="icon-color">Technologies </span>
                             </h3>
                         </div>
 
-                        <div class="row text-center justify-content-center">
+                        <div className="row text-center justify-content-center">
 
 
-                            <div class="col-sm-3 col-3 icon-text-style">
+                            <div className="col-sm-3 col-3 icon-text-style">
                                 <FontAwesomeIcon icon={faAndroid} size='3x' />
-                                {/* <i class="fab fa-android icon-color"></i> */}
+                                {/* <i className="fa fa-android icon-color"></i> */}
                                 <p>Android</p>
                             </div>
 
-                            <div class="col-sm-3 col-3 icon-text-style">
+                            <div className="col-sm-3 col-3 icon-text-style">
                                 <FontAwesomeIcon icon={faAppleAlt} size='3x' />
-                                {/* <i class="fab fa-apple icon-color"></i> */}
+                                {/* <i className="fa fa-apple icon-color"></i> */}
                                 <p>iOS</p>
                             </div>
 
-                            <div class="col-sm-3 col-3 icon-text-style  border-right-0">
+                            <div className="col-sm-3 col-3 icon-text-style  border-right-0">
                                 <FontAwesomeIcon icon={faReact} size='3x' />
+                                {/* <i className="fa fa- icon-color"></i> */}
                                 <p>React JS </p>
                             </div>
 
-                            <div class="col-sm-3 col-3 icon-text-style icon-text-style-2">
+                            <div className="col-sm-3 col-3 icon-text-style icon-text-style-2">
                                 <FontAwesomeIcon icon={faLaravel} size='3x' />
+                                {/* <i className="fa fa-laravel icon-color"></i> */}
                                 <p>Laravel</p>
                             </div>
 
-                            <div class="col-sm-3 col-3 icon-text-style icon-text-style-2">
+                            <div className="col-sm-3 col-3 icon-text-style icon-text-style-2">
                                 <FontAwesomeIcon icon={faPhp} size='3x' />
-                                {/* <i class="fab fa-php icon-color"></i> */}
+                                {/* <i className="fa fa-php icon-color"></i> */}
                                 <p>PHP</p>
                             </div>
 
-                            <div class="col-sm-3 col-3 icon-text-style icon-text-style-2 border-right-0">
+                            <div className="col-sm-3 col-3 icon-text-style icon-text-style-2 border-right-0">
                                 <FontAwesomeIcon icon={faDatabase} size='3x' />
-                                {/* <i class="fas fa-database icon-color"></i> */}
+                                {/* <i className="fa fa-database icon-color"></i> */}
                                 <p>SQL</p>
                             </div>
-                            <div class="col-sm-3 col-3 icon-text-style icon-text-style-2 border-right-0">
+                            <div className="col-sm-3 col-3 icon-text-style icon-text-style-2 border-right-0">
                                 <FontAwesomeIcon icon={faJs} size='3x' />
-                                {/* <i class="fab fa-js icon-color"></i> */}
+                                {/* <i className="fa fa-js icon-color"></i> */}
                                 <p>Javascript</p>
                             </div>
-                            <div class="col-sm-3 col-3 icon-text-style icon-text-style-2 border-right-0">
+                            <div className="col-sm-3 col-3 icon-text-style icon-text-style-2 border-right-0">
                                 <FontAwesomeIcon icon={faWordpress} size='3x' />
-                                {/* <i class="fab fa-wordpress icon-color"></i> */}
+                                {/* <i className="fa fa-wordpress icon-color"></i> */}
                                 <p>Wordpress</p>
                             </div>
                         </div>
